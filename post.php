39200e742b9e6ea92f4ecddd133d7acba2b27f7f
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2022-10-10 18:15:16 +0700

    Buat Profile
    Co-authored-by: Jef <Ajeppp@users.noreply.github.com>
    Co-authored-by: Steve Christian <stevgamz@users.noreply.github.com>

--- post.php
+++ post.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+require_once 'utilities.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +11,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Post</title>
 </head>
 
 <body>
-    <h2>ini post</h2>
+    <div class="justify-content-center">
+        <h2>Post</h2>
+    </div>
+    <div class="container">
+        <form action="utilities.php" method="post">
+            <div class="form-group mt-3">
+                <label for="nama-category">Topic</label>
+                <select class="form-control" name="category" required>
+                    <option value="">-- Topic --</option>
+                    <option value="php">PHP</option>
+                    <option value="python">PYTHON</option>
+                    <option value="javascript">JAVASCRIPT</option>
+                    <option value="c++">C++</option>
+                    <option value="c#">C#</option>
+                </select>
+            </div>
+
+            <div class="form-group mt-3">
+                <label for="nama-content">Comment</label>
+                <textarea class="form-control" name="content">
+                </textarea>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" name="login">Post</button>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
